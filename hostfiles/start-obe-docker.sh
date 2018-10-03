4d23650338731fee236ed6d1ea5cab0929ff64c9
pmw <kevin@polarismediaworks.com>
2018-10-03 01:53:27 -0400

    sudo for srt docker runs

--- hostfiles/start-obe-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-docker rm -f obe-rt
-docker run --name="obe-rt" --network="split" --ip="10.0.10.2" -v /home/kevin/apps/srt-connect-2/hostfiles:/home/hostfiles --privileged -i -t -d --device /dev/blackmagic/io0 pmw1/obe-rt 

539c4100f035d45fb05957ba72df1240783ebd66
pmw <kevin@polarismediaworks.com>
2018-10-04 02:17:33 -0400

    fixed perms

--- hostfiles/srt-entrypoint.sh
+++ hostfiles/srt-entrypoint.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-<<<<<<< HEAD
 /home/srt/srt-live-transmit -s:2000 -r:2000 -v srt://0.0.0.0:3000/?mode=server udp://10.0.10.5:4444
-=======
-/home/srt/srt-live-transmit -r:2000 -s:2000 -v udp://0.0.0.0:4443/?mode=client srt://69.204.169.118:3000
->>>>>>> 74db9cf2831c2c545cf263e8806bcdc6edde140e
--- hostfiles/start-obe-docker.sh
+++ hostfiles/start-obe-docker.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 sudo docker rm -f obe-rt
-<<<<<<< HEAD
 sudo docker run --name="obe-rt" --network="split" --ip="10.0.10.2" -v /home/pmw/apps/srt-connect-2/hostfiles:/home/hostfiles --privileged -i -t -d --device /dev/blackmagic/io0 pmw1/obe-rt 
-=======
-sudo docker run --name="obe-rt" --network="split" --ip="10.0.10.2" -v /home/kevin/apps/srt-connect-2/hostfiles:/home/hostfiles --privileged -i -t -d --device /dev/blackmagic/io0 pmw1/obe-rt 
->>>>>>> 74db9cf2831c2c545cf263e8806bcdc6edde140e

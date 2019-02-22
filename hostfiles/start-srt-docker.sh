4c5eb05f4e93156cb46d371acb8265aa86ab3806
Kevin Hurley <kevin@polarismediaworks.com>
2019-02-22 03:10:44 -0500

    fixed permission setup in setup.sh / .gitignore modfied
    Former-commit-id: 375b32c456b1421e45fca7def43d68cbe5aa9b56 [formerly 62aac35cf01f8ed4395f43cc79f7802de6cd5560]
    Former-commit-id: 4f0a5050d5ffbd72bae3e984e21df5cbb5e3d2e8

--- hostfiles/start-srt-docker.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-sudo docker rm -f srt-rx
-sudo docker run --name="srt-rx" --network="split" --ip="10.0.10.4" -p 3000:3000/udp -v /home/pmw/apps/srt-connect-2/hostfiles:/home/hostfiles --privileged -i -t -d --entrypoint="/home/hostfiles/srt-entrypoint.sh" pmw1/srt 

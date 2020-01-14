582769f4a62ab0c3f49155e433247838dde5a7de
Polaris Broadcasting <pmw@pmw-alb-2.pmw>
2020-01-14 02:24:00 -0500

    multicast and seperated process control for encode and srt

--- hostfiles/start-multicat.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-docker rm -f multicat
-docker run -v /home/pmw/apps/srt-connect-2/hostfiles/:/hostfiles --name="multicat" --network="split" --ip="10.0.10.3" --entrypoint="/bin/bash" --privileged -i -t -d pmw1/multicat
-echo && echo
